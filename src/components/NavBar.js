d2fd95c4db313fe6b11b6c901579b1c36fe9bd4e
[kekojeda] <[kekojeda@gmail.com]>
2022-06-23 16:39:44 -0300

    Se agrego NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+
+function NavBar(){
+
+   return(
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">
+      TecnoShop
+</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul className="navbar-nav mx-auto">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Categorias</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Ofertas</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Devoluciones</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Contacto</a>
+          </li>
+          
+          
+        </ul>
+        
+      </div>
+      
+      <button type="button" className="btn btn-outline-dark">Login</button>
+
+    </div>
+  </nav>
+    )
+}
+
+export { NavBar };
